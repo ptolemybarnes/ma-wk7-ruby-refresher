7f13bfad20de780b825419fcc259fd2ee9ac5f4c
Ptolemy <ptolemybarnes@gmail.com>
2015-02-09 08:57:56 +0000

    Corrected formatting of lyrics of 99 Bottles.

--- spec/99_bottles_spec.rb
+++ spec/99_bottles_spec.rb
@@ -3,14 +3,14 @@ require './lib/99_bottles'
 describe '99 bottles' do
 
   it 'returns lyrics when bottles plural' do
-    expect(99.bottles_lyric).to eq "99 bottles of beer on the wall, 99 bottles of beer. \n    Take one down and pass it around, 98 bottles of beer on the wall."
+    expect(99.bottles_lyric).to eq "99 bottles of beer on the wall, 99 bottles of beer. \n  Take one down and pass it around, 98 bottles of beer on the wall."
   end
 
   it 'returns lyrics with singular "bottle" when only 1 bottle' do
-    expect(1.bottles_lyric).to eq "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall."
+    expect(1.bottles_lyric).to eq "1 bottle of beer on the wall, 1 bottle of beer. \n  Take one down and pass it around, no more bottles of beer on the wall."
   end
 
   it 'returns end to lyrics when there are no more bottles' do
-    expect(0.bottles_lyric).to eq "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+    expect(0.bottles_lyric).to eq "No more bottles of beer on the wall, no more bottles of beer. \n  Go to the store and buy some more, 99 bottles of beer on the wall."
   end
 end
